ddaa51767fdc24bdff22f6881107384765df223e
Ranjithkumar7200 <ranjithkumarb8072@gmail.com>
2023-12-02 15:52:49 +0530

    spinner has changed

--- src/pages/loader/Loader.js
+++ src/pages/loader/Loader.js
@@ -8,7 +8,7 @@ import { Container } from "react-bootstrap";
     <Container className="vh-100 d-flex flex-column flex-wrap-wrap justify-content-center align-items-center">
   <InfinitySpin 
   width='200'
-  color="#00d4ff"
+  color="#5046e5"
 />
   </Container>
 );
